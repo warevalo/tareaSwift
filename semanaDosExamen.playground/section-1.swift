dde2697c278a959071850c7226164403ceb2a880
warevalo <warevalo.ipn@gmail.com>
2016-09-13 20:17:17 -0500

    Reto de la semana 2
    Reto de la semana 2

---
+++ semanaDosExamen.playground/section-1.swift
@@ -0,0 +1,24 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var numeros:[Int] = [4, 10, 9, 11, 10, 8]
+
+numeros[0] = numeros[0] + 2
+
+numeros[2] = numeros[2] + 2
+
+numeros[4] = numeros[4] + 2
+
+numeros
+
+
+var monedas = [17.17, 20.01, 15.9, 9.0, 111.2, 8.8]
+
+
+var baseDeDatos = ["LOO96":"David","L0097":"Luke","L0098":"Han","L100":"Clark"]
+
+baseDeDatos["L0098"]
+
+
+
